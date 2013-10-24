b4e98d0051595dbc220710611a846e32fca4a4d7
Jiakuan Wang <delight.wjk@gmail.com>
2013-10-24 21:41:53 +0800

    Added OnUnexpectedExit bindings

--- test_wx_and_event_1.py
+++ test_wx_and_event_1.py
@@ -1,4 +1,3 @@
-
 """PyObjC keylogger for Python
 by  ljos https://github.com/ljos
 """
@@ -17,16 +16,16 @@ class DemoTaskBarIcon(wx.TaskBarIcon):
     TBMENU_CLOSE = wx.NewId()
     TBMENU_CHANGE = wx.NewId()
     TBMENU_REMOVE = wx.NewId()
- 
+
     def __init__(self, frame):
         wx.TaskBarIcon.__init__(self)
         self.frame = frame
- 
+
         # Set the image
         icon = self.MakeIcon(icons.sample.GetImage())
         self.SetIcon(icon, "wxPython Sample")
         self.imgidx = 1
-        
+
         # bind some events
         self.Bind(wx.EVT_TASKBAR_LEFT_DCLICK, self.OnTaskBarActivate)
         self.Bind(wx.EVT_MENU, self.OnTaskBarActivate, id=self.TBMENU_RESTORE)
@@ -43,8 +42,8 @@ class DemoTaskBarIcon(wx.TaskBarIcon):
         menu.Append(self.TBMENU_RESTORE, "Restore wxPython Sample")
         menu.Append(self.TBMENU_CLOSE, "Close wxPython Sample")
         return menu
- 
- 
+
+
     def MakeIcon(self, img):
         """
         The various platforms have different requirements for the
@@ -54,7 +53,7 @@ class DemoTaskBarIcon(wx.TaskBarIcon):
             img = img.Scale(16, 16)
         elif "wxGTK" in wx.PlatformInfo:
             img = img.Scale(22, 22)
-            
+
         # wxMac can be any size upto 128x128, so leave the source img alone....
         icon = wx.IconFromBitmap(img.ConvertToBitmap())
         return icon
@@ -65,21 +64,22 @@ class DemoTaskBarIcon(wx.TaskBarIcon):
         if not self.frame.IsShown():
             self.frame.Show(True)
         self.frame.Raise()
- 
- 
+
+
     def OnTaskBarClose(self, evt):
         wx.CallAfter(self.frame.Close)
- 
- 
+
+
 class MainFrame(wx.Frame):
     def __init__(self, parent):
         wx.Frame.__init__(self, parent, title="Hello World")
         self.tbicon = DemoTaskBarIcon(self)
         self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
- 
+
     def OnCloseWindow(self, evt):
         self.tbicon.Destroy()
         evt.Skip()
+
 
 def handler(event):
     NSLog(u"%@", event)
@@ -94,30 +94,44 @@ def handler(event):
         modifiers.append('COMMAND')
     NSLog(u"%@", modifiers)
 
+
 def initEventMonitor():
-    NSEvent.addGlobalMonitorForEventsMatchingMask_handler_(NSLeftMouseDownMask 
-                                                           | NSLeftMouseUpMask 
+    NSEvent.addGlobalMonitorForEventsMatchingMask_handler_(NSLeftMouseDownMask
+                                                           | NSLeftMouseUpMask
                                                            | NSRightMouseDownMask
-                                                           | NSRightMouseUpMask 
-                                                           | NSMouseMovedMask 
-                                                           | NSLeftMouseDraggedMask 
-                                                           | NSRightMouseDraggedMask 
-                                                           | NSMouseEnteredMask 
+                                                           | NSRightMouseUpMask
+                                                           | NSMouseMovedMask
+                                                           | NSLeftMouseDraggedMask
+                                                           | NSRightMouseDraggedMask
+                                                           | NSMouseEnteredMask
                                                            | NSMouseExitedMask
                                                            | NSScrollWheelMask
-                                                           | NSKeyDownMask 
+                                                           | NSKeyDownMask
                                                            | NSKeyUpMask
                                                            | NSControlKeyMask
                                                            | NSAlternateKeyMask
                                                            | NSCommandKeyMask
-                                                           | NSFlagsChangedMask, handler)
+                                                           | NSFlagsChangedMask,
+                                                           handler)
+
+
+def OnUnexpectedExit(event):
+    #
+    # Event handler that deals with the unexpected close event.
+    #
+    #        self.Exit()
+    wx.GetApp().ExitMainLoop()
 
 
 def main():
     app = wx.App(redirect=False)
+
+    app.Bind(wx.EVT_QUERY_END_SESSION, OnUnexpectedExit)
+    app.Bind(wx.EVT_END_SESSION, OnUnexpectedExit)
+
     frame = MainFrame(None)
     frame.Show(True)
-    
+
     initEventMonitor()
     app.MainLoop()
 
